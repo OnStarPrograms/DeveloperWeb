99a8342d67b93a7f3a1723b9798c55c61452e7aa
OnStarPrograms <124287152+OnStarPrograms@users.noreply.github.com>
2023-08-20 12:03:29 -0400

    Cleaned up files

--- Test/Encoder.py
+++
@@ -1,5 +0,0 @@
-import hashlib
-f = open('C:\\Users\\aiden\\Desktop\\CodingWIPs\\DevWebsite\\Downloads\\MailAutomater.zip', "rb")
-TheInput = str(f.read())
-# print(sha256(input_.encode('utf-8')).hexdigest())
-result = hashlib.sha256(TheInput.encode())
